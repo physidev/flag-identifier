a5b38c74a27df1d834c313c86585499f1145cfec
physidev <42456287+physidev@users.noreply.github.com>
2023-01-22 13:46:37 -0600

    Added identification code

--- identifier.js
+++ identifier.js
@@ -1,44 +1,65 @@
 let img1Element = document.getElementById('image1Src');
 let input1Element = document.getElementById('fileInput1');
-let img2Element = document.getElementById('image2Src');
-let input2Element = document.getElementById('fileInput2');
+
+const FLAG_WIDTH = 64;
+const FLAG_HEIGHT = 32;
+const SHEET_WIDTH = 8;
 
 input1Element.addEventListener('change', (e) => {
     img1Element.src = URL.createObjectURL(e.target.files[0]);
 }, false);
-input2Element.addEventListener('change', (e) => {
-    img2Element.src = URL.createObjectURL(e.target.files[0]);
-}, false);
 
-img2Element.onload = function () {
-    let src = cv.imread(img2Element);
+function identify(n) {
+    let flg = cv.imread(img1Element);
+    let flagsheet = cv.imread(document.getElementById('flagsheet'));
+    cv.imshow('canvasOutput', flagsheet);
+
+    lowestDistance = Infinity;
+    lowestDistImg = new cv.Mat();
+
+    for (let i = 0; i < n; i++) {
+        console.log(i);
+        let blr = flagsheet.roi(new cv.Rect(64*(i % SHEET_WIDTH), 32*Math.floor(i / SHEET_WIDTH), FLAG_WIDTH, FLAG_HEIGHT));
+        cv.imshow("canvasOutput", blr);
+
+        distanceSum = getDistance(flg, blr);
+        console.log(i, distanceSum);
+
+        if (distanceSum < lowestDistance) {
+            lowestDistance = distanceSum;
+            lowestDistImg = blr;
+        }
+    }
+
+    cv.imshow("canvasOutput", lowestDistImg);
+    document.getElementById('distance').innerText = 'Distance: ' + lowestDistance;
+
+    flg.delete(); flagsheet.delete();
+}
+
+function getDistance(flag, blur) {
     let dst = new cv.Mat();
     let ksize = new cv.Size(9, 9);
-    cv.GaussianBlur(src, dst, ksize, 0, 0, cv.BORDER_DEFAULT);
+    cv.GaussianBlur(blur, dst, ksize, 0, 0, cv.BORDER_DEFAULT);
 
-    let cmp = cv.imread(img1Element);
-
-    // find total distance between pixels
     let distanceSum = 0;
-    let width = Math.min(cmp.size().width, dst.size().width);
-    let height = Math.min(cmp.size().height, dst.size().height);
-    for(let r=0; r < height; r++) {
-        for(let c=0; c < width; c++) {
-            let s = cmp.ucharPtr(r, c);
+    let width = Math.min(flag.size().width, dst.size().width);
+    let height = Math.min(flag.size().height, dst.size().height);
+    for (let r = 0; r < height; r++) {
+        for (let c = 0; c < width; c++) {
+            let s = flag.ucharPtr(r, c);
             let t = dst.ucharPtr(r, c);
             distanceSum += Math.sqrt(
-                (s[0]-t[0])*(s[0]-t[0]) +
-                (s[1]-t[1])*(s[1]-t[1]) +
-                (s[2]-t[2])*(s[2]-t[2])
+                (s[0] - t[0]) * (s[0] - t[0]) +
+                (s[1] - t[1]) * (s[1] - t[1]) +
+                (s[2] - t[2]) * (s[2] - t[2])
             );
         }
     }
-    console.log(distanceSum);
-    document.getElementById('distance').innerText = 'Distance: ' + distanceSum;
 
-    cv.imshow('canvasOutput', dst);
-    src.delete(); dst.delete();
-};
+    dst.delete();
+    return distanceSum;
+}
 
 function onOpenCvReady() {
     document.getElementById('status').innerHTML = 'OpenCV.js is ready.';
